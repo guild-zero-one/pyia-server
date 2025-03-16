db28d9b9346c439c382268b9996a46e902a1903f
joaoGlima09 <joao.glima@sptech.school>
2025-03-16 18:03:32 -0300

    feat: :sparkles: modules

--- app/modules/boticario.py
+++ app/modules/boticario.py
@@ -1,9 +1,15 @@
 import pandas as pd
+from app.utils.pdf_transform import transform_df
 
 
-def text_to_dataframe(text):
-    lines = text.strip().split("\n")
+def create_df(content: str):
+    # Apagando texto desnecessário
+    remove_data = content.split("\nIPI\nICMS")
+    remove_data = remove_data[1].split("RESERVADO AO FISCO")
 
+    lines = remove_data[0].strip().split("\n")
+
+    # Colunas do DataFrame
     header = [
         "CÓD. PRODUTO",
         "DESCRIÇÃO DOS PRODUTOS/SERVIÇOS",
@@ -27,6 +33,6 @@ def text_to_dataframe(text):
         data.append(row)
 
     df = pd.DataFrame(data, columns=header)
-    df.to_string(buf="app/upload/p.txt", index=False)
+    transform_df(df)
 
     return df
