de0fff3d74d062e6adec0dbd96329cff3ab08861
joaoGlima09 <joao.glima@sptech.school>
2025-03-23 12:27:16 -0300

    style: :art: name txt

--- app/utils/pdf_transform.py
+++ app/utils/pdf_transform.py
@@ -4,5 +4,4 @@ import pandas as pd
 
 
 def transform_df(df: pd.DataFrame):
-    df.to_string(buf="app/upload/p.txt", index=False)
-    return df
+    df.to_string(buf="app/upload/products.txt", index=False)
