20d2bc957dd3982bd8c2c6df153320cb0fb385a9
joaoGlima09 <joao.glima@sptech.school>
2025-03-23 12:24:13 -0300

    feat: :sparkles: class PathManager

---
+++ app/manager/path_manager.py
@@ -0,0 +1,32 @@
+"""PathManager"""
+
+import os
+
+from fastapi import UploadFile
+
+
+class PathManager:
+    def __init__(self):
+        self.path_pdf = os.path.join(os.getcwd(), "app", "pdf")
+        self.path_product = os.path.join(
+            os.getcwd(), "app", "upload", "products.txt"
+        )
+        self.path_prompt = os.path.join(
+            os.getcwd(), "app", "gemini", "prompt.txt"
+        )
+
+    def read_path(self, path: str) -> str:
+        with open(path, "r", encoding="utf-8") as path_file:
+            return path_file.read()
+
+    def exists_path(self, path: str) -> bool:
+        return os.path.exists(path)
+
+    def create_path(self, path: str, file: UploadFile) -> str:
+        return os.path.join(path, file.filename)
+
+    def remove_path(self, path: str) -> bool:
+        if os.path.exists(path):
+            os.remove(path)
+            return True
+        return False
